59ab374b353543dddb12ec1fd75221b312397997
Anthony <anthony-andre@live.fr>
2022-09-15 10:25:46 +0200

    User feature

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/creer-un-compte', name: 'app_register')]
+    public function index(): Response
+    {
+
+        
+
+
+
+
+
+
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
+        ]);
+    }
+}
