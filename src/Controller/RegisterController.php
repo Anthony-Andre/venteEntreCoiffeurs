91f1336540d28ca6d59b6f71f5bbd8210cea7ccf
Anthony <anthony-andre@live.fr>
2022-09-15 11:23:09 +0200

    formulaire Register

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/creer-un-compte', name: 'app_register')]
+    #[Route('/creation-de-compte', name: 'app_register')]
     public function index(): Response
     {
 
-        
+        $user = new User; 
+        $form = $this->createForm(RegisterType::class, $user); 
+
+
+
 
 
 
@@ -21,6 +27,7 @@ class RegisterController extends AbstractController
 
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,12 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse mail :',
+                'attr' => [
+                    "placeholder" => "Adresse Email"
+                ]
+            ])
             ->add('password')
             ->add('firstname')
             ->add('lastname')
